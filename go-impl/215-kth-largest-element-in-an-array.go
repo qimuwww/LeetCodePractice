0c421ece5848dd0ea6cf98fe15e46162854c58ea
lambda <qimuwww@gmail.com>
2020-08-28 14:03:49 +0800

    215-kth-largest-element-in-an-array

--- go-impl/215-kth-largest-element-in-an-array.go
+++ go-impl/215-kth-largest-element-in-an-array.go
@@ -15,6 +15,49 @@ package main
 	You may assume k is always valid, 1 ≤ k ≤ array's length.
 */
 
+// 首先使用该数组构建一个大顶堆
+// 最后一个节点的父节点(n/2),从该节点开始对该大顶堆进行调整
+func findKthLargest2(nums []int, k int) int {
+	for i := len(nums)/2 - 1; i >= 0; i-- {
+		sink(&nums, i, len(nums))
+	}
+	for i := len(nums) - 1; i >= len(nums)-k; i-- {
+		nums[0], nums[i] = nums[i], nums[0]
+		sink(&nums, 0, i)
+	}
+	return nums[len(nums)-k]
+}
+
+// 数组中下标从0开始，所以在堆中i的左子结点为2i+1，右子结点为2i+2
+func left(parent int) int {
+	return parent*2 + 1
+}
+func right(parent int) int {
+	return parent*2 + 2
+}
+
+func max(heap []int, l, r int) bool {
+	return heap[l] > heap[r]
+}
+
+func sink(heap *[]int, k, l int) {
+	for {
+		if left(k) <= l-1 {
+			older := left(k)
+			if right(k) <= l-1 && max(*heap, right(k), older) {
+				older = right(k)
+			}
+			if max(*heap, k, older) {
+				break
+			}
+			(*heap)[k], (*heap)[older] = (*heap)[older], (*heap)[k]
+			k = older
+		} else {
+			break
+		}
+	}
+}
+
 // 对nums数组进行降序快排
 // 排序期间判断是否已经对k-1位置的数排序完成
 // 随后只进行部份快排
